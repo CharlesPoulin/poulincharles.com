15fdabfc27012ea18a67289a0a92e952e8760152
Charles Poulin <74792298+CharlesPoulin@users.noreply.github.com>
2023-09-02 16:47:53 -0400

    Profile Picture fix
    seems like the path could be radius and the style blur was just making it disappear.
    
    also : priority default is true

--- app/components/intro.tsx
+++ app/components/intro.tsx
@@ -6,7 +6,7 @@ export default function Intro() {
     <section>
         <div className="flex items-center justify-center">
             <div>
-                <Image className="object-cover h-full w-full blur-[10rem] sm:w-[68.75rem]" src="/app/public/profilePicture.png" alt="Charles Poulin" width={192} height={192} quality={95} priority={true}/>
+                <Image className=" object-cover h-full w-full sm:w-[68.75rem]" src="/profilePicture.png" alt="Charles Poulin" width={192} height={192} quality={95} priority/>
             </div>
             </div>
     </section>
