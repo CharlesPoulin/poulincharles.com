6ca69035c490c0ac7fef2a97ae9c302f45c2a31a
Charles Poulin <74792298+CharlesPoulin@users.noreply.github.com>
2023-09-02 19:02:08 -0400

    Contact button
    Added button To href to contact me, download cv, linknd and github. (!important I dedcided to make them open a new page to revist i'm not sure if that the better UX for me it is )
    
    When its push it now build in vercel and hosted.
    
    its also linked to poulincharles.com So the user now sees my website

--- app/components/intro.tsx
+++ app/components/intro.tsx
@@ -3,6 +3,10 @@
 import React from 'react'
 import Image from 'next/image'
 import { motion } from 'framer-motion'
+import Link from 'next/link';
+import { BsArrowRight, BsLinkedin } from 'react-icons/bs'
+import { HiDownload } from 'react-icons/hi'
+import { FaGithubSquare } from 'react-icons/fa';
 
 export default function Intro() {
   return (
@@ -33,6 +37,27 @@ export default function Intro() {
         <span className="italic"> Artificial Intelligence, Machine Learning, and Video Games</span>. and I&apos;m also deeply interested in the {" "}
         <span className="underline">Quantum Field</span>.
       </motion.h1>
+
+    <motion.div className='flex flex-col sm:flex-row justify-center gap-2 px-4 text-lg font-medium'
+    initial={{ opacity: 0, y: 100}}
+    animate={{ opacity: 1, y: 0}}
+    transition={{delay: 0.1}}>
+
+      <Link href="#contact" 
+      className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105
+      transition-all'
+      > Contact me here <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition' /> </Link>
+
+      <a className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105
+      transition-all cursor-pointer border border-black/10'  href="/CharlesPoulinCV.pdf" download > Download CV <HiDownload className='opacity-70 group-hover:translate-y-1 transistion'/></a>
+
+      <a className='group bg-white p-4 flex text-gray-700 items-center gap-2 rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950  active:scale-105
+      transition-all cursor-pointer border border-black/10' href='https://www.linkedin.com/in/charles-poulin/' target="_blank"> < BsLinkedin /> </a>
+
+      <a className='group bg-white p-4 flex text-gray-700 text-[1.35rem] items-center gap-2 rounded-full outline-none focus:scale-[1.15]  hover:scale-[1.15]  hover:text-gray-950 active:scale-105
+      transition-all cursor-pointer border border-black/10' href='https://github.com/CharlesPoulin' target="_blank"> < FaGithubSquare /> </a>
+
+    </motion.div>
 
     </section>
   )
