a12845880a364a47accb696dc51d84a1da08b54e
Charles Poulin <74792298+CharlesPoulin@users.noreply.github.com>
2023-09-03 19:24:14 -0400

    fix apos and not template content

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -19,31 +19,19 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
-        <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
-        </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
-      </p>
+  After gaining valuable experience through my first internship at Ubisoft, I knew that my <span className="font-medium">
+    passion for programming was undeniable.</span>
+     I thrive on challenges and problem-solving, and my core tech stack includes
+      <span className="font-medium">Python, C#, TypeScript, and C++</span>. 
+      I&apos;m always ready to learn and adapt to new technologies. Currently, 
+      I&apos;m seeking opportunities to contribute my skills and creativity as a software developer.
 
-      <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
-      </p>
+  <span className="italic">When I&apos;m not immersed in code</span>, 
+  you&apos;ll find me exploring the fascinating world of Quantum AI 
+  or indulging my love for video games. I believe that the intersection of
+   AI and philosophy holds incredible potential, and I&apos;m eager to delve deeper into this realm.
+    Let&apos;s create something <span className="font-medium">remarkable</span> together!
+</p>
     </motion.section>
   );
 }
