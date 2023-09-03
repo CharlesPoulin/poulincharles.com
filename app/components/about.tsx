dd1ade10b021c0891f4bc54186ebb893a2eaa84f
Charles Poulin <74792298+CharlesPoulin@users.noreply.github.com>
2023-09-02 20:20:23 -0400

    Subdivider + about me
     I need to make the content that goes into about me aka text :)

---
+++ app/components/about.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React from 'react'
+import SectionHeading from './section-heading';
+import { motion } from 'framer-motion';
+
+export default function About() {
+  return (
+    <motion.section className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40'
+    initial={{ opacity: 0, y: 100 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{delay: 0.2}}>
+      <p className="mb-4">
+        <SectionHeading>About me</SectionHeading>
+        I am currently studying{" "}
+        <span className="font-medium">Software engineering</span>, In Quebec, Canada{" "}
+        <span className="italic">My favorite part of programming</span>
+     
+      </p>
+    </motion.section>
+  )
+}
